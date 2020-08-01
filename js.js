685e17de31e88f2701a7292c6d57f89bf9547cc4
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2020-08-01 13:39:46 +0530

    Adding Functions

--- js.js
+++ js.js
@@ -1,3 +1,20 @@
 window.onload=function(){
-    alert();
+    chrome.history.search({text:""}, function(data){
+        var i;
+        for(i=0; i<data.length; i++)
+        {
+            var title = data[i].title;
+            var url = data[i].url;
+        
+            var heading = document.createElement("H4");
+            heading.innerHTML = title;
+
+            var link = document.createElement("A");
+            link.innerHTML=url;
+            link.href=url;
+
+            document.body.append(heading);
+            document.body.append(link);
+        }
+    });
 }
